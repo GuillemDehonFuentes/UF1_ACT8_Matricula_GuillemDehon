8ffe29bd1d7a0f255df751decff02ad68b16bd75
GuillemDehonFuentes <156358165+GuillemDehonFuentes@users.noreply.github.com>
2024-10-22 20:16:27 +0200

    Add files via upload

---
+++ UF1_ACT8_Matriculaccio_GuillemDehon/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UF1_ACT8_Matriculaccio_GuillemDehon"
+include(":app")
+ 
